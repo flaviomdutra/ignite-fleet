c7cb2f9b5af8fce609acc5f841333e2d523dd2b7
Flávio Dutra <flaviomdutra@outlook.com>
2023-05-20 12:19:15 -0300

    feat: add logout

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -1,26 +1,36 @@
-import { TouchableOpacity } from 'react-native';
-import { Power } from 'phosphor-react-native';
+import { TouchableOpacity } from "react-native";
+import { Power } from "phosphor-react-native";
+import { useApp, useUser } from "@realm/react";
 
-import theme from '../../theme';
-import { Container, Greeting, Message, Name } from './styles';
+import theme from "../../theme";
+import { Container, Greeting, Message, Name, Picture } from "./styles";
 
 export function HomeHeader() {
+  const user = useUser();
+  const app = useApp();
+
+  function handleLogout() {
+    app.currentUser?.logOut();
+  }
+
   return (
     <Container>
-      <Greeting>
-        <Message>
-          Olá
-        </Message>
+      <Picture
+        source={{
+          uri: user?.profile.pictureUrl,
+        }}
+        placeholder="L184i9ofbHof00ayjsay~qj[ayj@"
+      />
 
-        <Name>
-          Rodrigo
-        </Name>
+      <Greeting>
+        <Message>Olá</Message>
+
+        <Name>{user?.profile.name}</Name>
       </Greeting>
 
-      <TouchableOpacity>
+      <TouchableOpacity activeOpacity={0.7} onPress={handleLogout}>
         <Power size={32} color={theme.COLORS.GRAY_400} />
       </TouchableOpacity>
-
     </Container>
   );
 }
