c293ce0ec47e73a2f83ffb3e71e679831489fe36
Flávio Dutra <flaviodutra@Daianes-MacBook-Pro.local>
2023-05-21 01:01:55 -0300

    feat: configure schemas on realm

--- App.tsx
+++ App.tsx
@@ -1,20 +1,21 @@
 import { StatusBar } from "react-native";
 import { AppProvider, UserProvider } from "@realm/react";
 import { ThemeProvider } from "styled-components";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
 
-import theme from "./src/theme";
+import { Routes } from "./src/routes";
+import { RealmProvider } from "./src/libs/realm";
 
 import { REALM_APP_ID } from "@env";
+import theme from "./src/theme";
 
 import { SignIn } from "./src/screens/SignIn";
 import { Loading } from "./src/components/Loading";
-import { Routes } from "./src/routes";
-import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -36,7 +37,9 @@ export default function App() {
             translucent
           />
           <UserProvider fallback={SignIn}>
-            <Routes />
+            <RealmProvider>
+              <Routes />
+            </RealmProvider>
           </UserProvider>
         </SafeAreaProvider>
       </ThemeProvider>
