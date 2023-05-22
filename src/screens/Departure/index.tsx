9170fbecd8cbeebaf1b73ba0a795572d2a3fc756
Flávio Dutra <flaviomdutra@outlook.com>
2023-05-22 17:01:29 -0300

    feat: Departure screen

--- src/screens/Departure/index.tsx
+++ src/screens/Departure/index.tsx
@@ -1,5 +1,55 @@
-import { Container } from "./styles";
+import { useRef } from "react";
+import { Button } from "../../components/Button";
+import { Header } from "../../components/Header";
+import { LicensePlateInput } from "../../components/LicensePlateInput";
+import { TextAreaInput } from "../../components/TextAreaInput";
+import { Container, Content } from "./styles";
+import {
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  TextInput,
+} from "react-native";
+
+const keyboardAvoidingViewBehavior =
+  Platform.OS === "android" ? "height" : "position";
 
 export function Departure() {
-  return <Container></Container>;
+  const descriptionRef = useRef<TextInput>(null);
+
+  function handleDepartureRegister() {
+    console.log("handleDepartureRegister");
+  }
+
+  return (
+    <Container>
+      <Header title="Saída" />
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={keyboardAvoidingViewBehavior}
+      >
+        <ScrollView>
+          <Content>
+            <LicensePlateInput
+              label="Placa do veículo"
+              placeholder="BRA1234"
+              onSubmitEditing={() => descriptionRef.current?.focus()}
+              returnKeyType="next"
+            />
+
+            <TextAreaInput
+              ref={descriptionRef}
+              label="Finalidade"
+              placeholder="Vou utilizar o veículo para..."
+              onSubmitEditing={handleDepartureRegister}
+              returnKeyType="send"
+              blurOnSubmit
+            />
+
+            <Button title="Registrar Saída" onPress={handleDepartureRegister} />
+          </Content>
+        </ScrollView>
+      </KeyboardAvoidingView>
+    </Container>
+  );
 }
