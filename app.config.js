42a9ed06e5cbe0742182d7f1e500c0aca8c04fae
Flávio Dutra <flaviomdutra@outlook.com>
2023-07-02 14:28:54 -0300

    feat: install lib to maps

--- app.config.js
+++ app.config.js
@@ -1,4 +1,8 @@
-{
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+module.exports = {
   "expo": {
     "name": "ignitefleet",
     "slug": "ignitefleet",
@@ -17,14 +21,22 @@
     ],
     "ios": {
       "supportsTablet": true,
-      "bundleIdentifier": "com.flaviomdutra.ignitefleet"
+      "bundleIdentifier": "com.flaviomdutra.ignitefleet",
+      "config": {
+        "googleMapsApiKey": process.env.GOOGLE_MAPS_API_KEY
+      }
     },
     "android": {
       "adaptiveIcon": {
         "foregroundImage": "./assets/adaptive-icon.png",
         "backgroundColor": "#202024"
       },
-      "package": "com.flaviomdutra.ignitefleet"
+      "package": "com.flaviomdutra.ignitefleet",
+      "config": {
+        "googleMaps": {
+          "apiKey": process.env.GOOGLE_MAPS_API_KEY
+        }
+      }
     },
     "web": {
       "favicon": "./assets/favicon.png"
